7a5851da7eb5e0c84b61e1a6966616e56d52109f
26oglel <26oglel@elida.k12.oh.us>
2024-01-12 10:22:30 -0500

    11

---
+++ Assets/DoorController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    public GameObject Door;
+    public bool doorIsOpen;
+
+/*    private void OnTriggerEnter(Collider other)
+    {
+        if(doorIsOpen == true)
+        {
+            Door.SetActive = true;
+        }
+        else
+        {
+            Door.SetActive = false;
+        }
+    }*/
+    void Update()
+    {
+
+    }
+}
